6c8c70273e734039e4e300e8f5047feb469348f3
mhmadidris <idris3097@gmail.com>
2024-04-04 23:39:33 +0700

    Add Login & Logout Feature

--- src/main/java/com/bengkel/booking/services/BengkelService.java
+++ src/main/java/com/bengkel/booking/services/BengkelService.java
@@ -1,20 +1,49 @@
 package com.bengkel.booking.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.bengkel.booking.models.Customer;
 import com.bengkel.booking.models.MemberCustomer;
 
 public class BengkelService {
-	
-	//Silahkan tambahkan fitur-fitur utama aplikasi disini
-	
-	//Login
-	
-	//Info Customer
-	
-	//Booking atau Reservation
-	
-	//Top Up Saldo Coin Untuk Member Customer
-	
-	//Logout
-	
+	private static int loginCount = 0;
+
+	// Silahkan tambahkan fitur-fitur utama aplikasi disini
+
+	// Login
+	public static boolean loginService(String customerID, String customerPassword, List<Customer> listAllCustomers) {
+		if (loginCount >= 3) {
+			System.out.println("Alert: Maaf, Anda terlalu banyak percobaan");
+			System.exit(0);
+		}
+
+		Optional<Customer> checkCustomer = listAllCustomers.stream()
+				.filter(customer -> customer.getCustomerId().equals(customerID))
+				.findFirst();
+		if (checkCustomer.isPresent()) {
+			Customer customer = checkCustomer.get();
+			if (customer.getPassword().equals(customerPassword)) {
+				System.out.println("Login Berhasil");
+				return true;
+			} else {
+				System.out.println("Alert: Maaf Password Yang Anda Masukkan Salah!");
+				loginCount++;
+				return false;
+			}
+		} else {
+			System.out.println("Alert: Maaf, Customer Tidak Ditemukan atau Salah!");
+			loginCount++;
+			return false;
+		}
+	}
+
+	// Info Customer
+
+	// Booking atau Reservation
+
+	// Top Up Saldo Coin Untuk Member Customer
+
+	// Logout
+
 }
--- src/main/java/com/bengkel/booking/services/MenuService.java
+++ src/main/java/com/bengkel/booking/services/MenuService.java
@@ -1,7 +1,10 @@
 package com.bengkel.booking.services;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import com.bengkel.booking.models.Customer;
 import com.bengkel.booking.models.ItemService;
@@ -12,51 +15,61 @@ public class MenuService {
 	private static List<Customer> listAllCustomers = CustomerRepository.getAllCustomer();
 	private static List<ItemService> listAllItemService = ItemServiceRepository.getAllItemService();
 	private static Scanner input = new Scanner(System.in);
+
 	public static void run() {
 		boolean isLooping = true;
 		do {
 			login();
-			mainMenu();
+			// mainMenu();
 		} while (isLooping);
-		
+
 	}
-	
+
 	public static void login() {
-		
+		System.out.print("Masukkan Customer ID: ");
+		String inputCustomerID = input.nextLine();
+		System.out.print("Masukkan Password: ");
+		String inputCustomerPassword = input.nextLine();
+
+		Boolean bengkelService = BengkelService.loginService(inputCustomerID, inputCustomerPassword, listAllCustomers);
+		if (bengkelService) {
+			mainMenu();
+		}
 	}
-	
+
 	public static void mainMenu() {
-		String[] listMenu = {"Informasi Customer", "Booking Bengkel", "Top Up Bengkel Coin", "Informasi Booking", "Logout"};
+		String[] listMenu = { "Informasi Customer", "Booking Bengkel", "Top Up Bengkel Coin", "Informasi Booking",
+				"Logout" };
 		int menuChoice = 0;
 		boolean isLooping = true;
-		
+
 		do {
 			PrintService.printMenu(listMenu, "Booking Bengkel Menu");
-			menuChoice = Validation.validasiNumberWithRange("Masukan Pilihan Menu:", "Input Harus Berupa Angka!", "^[0-9]+$", listMenu.length-1, 0);
+			menuChoice = Validation.validasiNumberWithRange("Masukan Pilihan Menu:", "Input Harus Berupa Angka!",
+					"^[0-9]+$", listMenu.length - 1, 0);
 			System.out.println(menuChoice);
-			
+
 			switch (menuChoice) {
-			case 1:
-				//panggil fitur Informasi Customer
-				break;
-			case 2:
-				//panggil fitur Booking Bengkel
-				break;
-			case 3:
-				//panggil fitur Top Up Saldo Coin
-				break;
-			case 4:
-				//panggil fitur Informasi Booking Order
-				break;
-			default:
-				System.out.println("Logout");
-				isLooping = false;
-				break;
+				case 1:
+					// panggil fitur Informasi Customer
+					break;
+				case 2:
+					// panggil fitur Booking Bengkel
+					break;
+				case 3:
+					// panggil fitur Top Up Saldo Coin
+					break;
+				case 4:
+					// panggil fitur Informasi Booking Order
+					break;
+				default:
+					System.out.println("Logout");
+					isLooping = false;
+					break;
 			}
 		} while (isLooping);
-		
-		
+
 	}
-	
-	//Silahkan tambahkan kodingan untuk keperluan Menu Aplikasi
+
+	// Silahkan tambahkan kodingan untuk keperluan Menu Aplikasi
 }
